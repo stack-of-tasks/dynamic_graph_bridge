22f4ede91ba8db7d2b26be4041bf8ad7ef2be6aa
Thomas Moulard <thomas.moulard@gmail.com>
2011-11-14 19:32:55 +0100

    Add missing files.

---
+++ src/ros_init.cpp
@@ -0,0 +1,24 @@
+#include <boost/make_shared.hpp>
+#include <boost/shared_ptr.hpp>
+
+#include "ros_init.hh"
+
+namespace dynamicgraph
+{
+  boost::shared_ptr<ros::NodeHandle> nodeHandle;
+
+  ros::NodeHandle& rosInit()
+  {
+    if (!nodeHandle)
+      {
+	int argc = 1;
+	char* arg0 = strdup("dynamic_graph_bridge");
+	char* argv[] = {arg0, 0};
+	ros::init(argc, argv, "dynamic_graph_bridge");
+	free (arg0);
+
+	nodeHandle = boost::make_shared<ros::NodeHandle> ("dynamic_graph");
+      }
+    return *nodeHandle;
+  }
+} // end of namespace dynamicgraph.
---
+++ src/ros_init.hh
@@ -0,0 +1,11 @@
+#ifndef ROS_INIT_HH
+# define ROS_INIT_HH
+# include <ros/ros.h>
+
+namespace dynamicgraph
+{
+  ros::NodeHandle& rosInit();
+
+} // end of namespace dynamicgraph.
+
+#endif //! ROS_INIT_HH
