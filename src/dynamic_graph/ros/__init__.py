ee35543ca7450165c2f84e52bca27fdec7f3d763
Francois Keith <keith@lirmm.fr>
2013-09-25 23:52:57 +0200

    Rename ros_import into ros_publish, and ros_export into ros_subscribe.
    The old names are kept for now, but will disappear in the
    future.

--- src/dynamic_graph/ros/__init__.py
+++ src/dynamic_graph/ros/__init__.py
@@ -1,6 +1,12 @@
-from ros_import import RosImport
-from ros_export import RosExport
+from ros_publish import RosPublish
+from ros_subscribe import RosSubscribe
 from ros_joint_state import RosJointState
 from robot_model import RosRobotModel
 
 from ros import Ros
+
+# aliases, for retro compatibility
+ros_import = ros_publish
+ros_export = ros_subscribe
+from ros import RosPublish as RosImport
+from ros import RosSubscribe as RosExport
