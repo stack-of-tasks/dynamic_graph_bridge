aadf42c6395a812b05d22e594f0fad5867e6267b
Joseph Mirabel <jmirabel@laas.fr>
2018-09-26 19:35:24 +0200

    Remove RosJointState from Python files

--- src/dynamic_graph/ros/__init__.py
+++ src/dynamic_graph/ros/__init__.py
@@ -1,3 +1,8 @@
 from ros_publish import RosPublish
 from ros_subscribe import RosSubscribe
 
+# aliases, for retro compatibility
+from ros import RosPublish as RosImport
+from ros import RosSubscribe as RosExport
+
+
