59de63586703cead9b095a3b22b8cfd92f30400f
Joseph Mirabel <jmirabel@laas.fr>
2018-09-28 17:19:33 +0200

    Enable real-time logs

--- src/geometric_simu.cpp
+++ src/geometric_simu.cpp
@@ -19,11 +19,18 @@
 #include <boost/thread/thread.hpp>
 #include <boost/thread/condition.hpp>
 
+#include <iostream>
+
+#define ENABLE_RT_LOG
+#include <dynamic-graph/real-time-logger.h>
+
 #include <dynamic_graph_bridge/sot_loader.hh>
 
 
 int main(int argc, char *argv[])
 {
+  dgADD_OSTREAM_TO_RTLOG (std::cerr);
+
   ros::init(argc, argv, "sot_ros_encapsulator");
   SotLoader aSotLoader;
   if (aSotLoader.parseOptions(argc,argv)<0)
