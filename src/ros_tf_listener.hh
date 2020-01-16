50c83963a543802907424015a6870625b9941381
Joseph Mirabel <jmirabel@laas.fr>
2020-01-16 17:30:49 +0100

    [TF2] Add a signal that says whether the transform is available.

--- src/ros_tf_listener.hh
+++ src/ros_tf_listener.hh
@@ -18,16 +18,18 @@ class RosTfListener;
 
 namespace internal {
 struct TransformListenerData {
-  typedef SignalTimeDependent<sot::MatrixHomogeneous, int> signal_t;
-  typedef SignalPtr<sot::MatrixHomogeneous, int> signalIn_t;
+  typedef SignalTimeDependent<bool, int> AvailableSignal_t;
+  typedef SignalTimeDependent<sot::MatrixHomogeneous, int> MatrixSignal_t;
+  typedef SignalPtr<sot::MatrixHomogeneous, int> DefaultSignal_t;
 
   RosTfListener* entity;
   tf2_ros::Buffer& buffer;
   const std::string toFrame, fromFrame;
   geometry_msgs::TransformStamped transform;
   ros::Duration max_elapsed;
-  signal_t signal;
-  signalIn_t failbackSig;
+  AvailableSignal_t availableSig;
+  MatrixSignal_t signal;
+  DefaultSignal_t failbackSig;
 
   TransformListenerData(RosTfListener* e, tf2_ros::Buffer& b,
                         const std::string& to, const std::string& from,
@@ -37,16 +39,24 @@ struct TransformListenerData {
       , toFrame(to)
       , fromFrame(from)
       , max_elapsed(0.5)
+      , availableSig(signame+"_available")
       , signal(signame)
       , failbackSig(NULL, signame+"_failback")
   {
     signal.setFunction(
         boost::bind(&TransformListenerData::getTransform, this, _1, _2));
+
+    availableSig.setFunction(
+        boost::bind(&TransformListenerData::isAvailable, this, _1, _2));
+    availableSig.setNeedUpdateFromAllChildren(true);
+
     failbackSig.setConstant (sot::MatrixHomogeneous::Identity());
-    signal.addDependency(failbackSig);
+    signal.addDependencies(failbackSig << availableSig);
   }
 
   sot::MatrixHomogeneous& getTransform(sot::MatrixHomogeneous& res, int time);
+
+  bool& isAvailable(bool& isAvailable, int time);
 };
 }  // namespace internal
 
@@ -82,11 +92,13 @@ class RosTfListener : public Entity {
     addCommand("setMaximumDelay", command::makeCommandVoid2(*this, &RosTfListener::setMaximumDelay, docstring));
   }
 
-  ~RosTfListener() {
+  ~RosTfListener()
+  {
     for (Map_t::const_iterator _it = listenerDatas.begin(); _it != listenerDatas.end(); ++_it) delete _it->second;
   }
 
-  void add(const std::string& to, const std::string& from, const std::string& signame) {
+  void add(const std::string& to, const std::string& from, const std::string& signame)
+  {
     if (listenerDatas.find(signame) != listenerDatas.end())
       throw std::invalid_argument("A signal " + signame + " already exists in RosTfListener " + getName());
 
@@ -95,7 +107,7 @@ class RosTfListener : public Entity {
 
     TransformListenerData* tld =
         new TransformListenerData(this, buffer, to, from, signalName.str());
-    signalRegistration(tld->signal << tld->failbackSig);
+    signalRegistration(tld->signal << tld->availableSig << tld->failbackSig);
     listenerDatas[signame] = tld;
   }
 
