be845c9fb775ff7d966ab800c6dcc49adb179e8a
Olivier Stasse <ostasse@laas.fr>
2020-06-23 19:17:13 +0200

    At initialization time read the ros param /robot_description and record it in the sot parameter-server.

---
+++ include/dynamic_graph_bridge/ros_parameter.hh
@@ -0,0 +1,9 @@
+#ifndef _ROS_DYNAMIC_GRAPH_PARAMETER_
+#define _ROS_DYNAMIC_GRAPH_PARAMETER_
+
+namespace dynamicgraph {
+
+bool parameter_server_read_robot_description();
+
+};
+#endif /* _ROS_DYNAMIC_GRAPH_PARAMETER_ */
