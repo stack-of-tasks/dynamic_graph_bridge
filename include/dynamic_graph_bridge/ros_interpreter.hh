d4137a9914acf2a4af3c572ccda41bc71126f01e
Olivier Stasse <olivier.stasse@laas.fr>
2021-06-14 04:48:02 +0000

    Update ros_interpreter to ROS2.

--- include/dynamic_graph_bridge/ros_interpreter.hh
+++ include/dynamic_graph_bridge/ros_interpreter.hh
@@ -2,12 +2,15 @@
 #define DYNAMIC_GRAPH_BRIDGE_INTERPRETER_HH
 #pragma GCC diagnostic push
 #pragma GCC system_header
-#include <ros/ros.h>
+//#include <ros/ros.h>
+#include <rclcpp/rclcpp.hpp>
 #pragma GCC diagnostic pop
 
-#include <dynamic_graph_bridge_msgs/RunCommand.h>
-#include <dynamic_graph_bridge_msgs/RunPythonFile.h>
+#include <dynamic_graph_bridge_msgs/srv/run_command.hpp>
+#include <dynamic_graph_bridge_msgs/srv/run_python_file.hpp>
 #include <dynamic-graph/python/interpreter.hh>
+
+#include <boost/function.hpp>
 
 namespace dynamicgraph {
 /// \brief This class wraps the implementation of the runCommand
@@ -18,15 +21,15 @@ namespace dynamicgraph {
 /// the outside.
 class Interpreter {
  public:
-  typedef boost::function<bool(dynamic_graph_bridge_msgs::RunCommand::Request&,
-                               dynamic_graph_bridge_msgs::RunCommand::Response&)>
+  typedef boost::function<bool(dynamic_graph_bridge_msgs::srv::RunCommand::Request&,
+                               dynamic_graph_bridge_msgs::srv::RunCommand::Response&)>
       runCommandCallback_t;
 
-  typedef boost::function<bool(dynamic_graph_bridge_msgs::RunPythonFile::Request&,
-                               dynamic_graph_bridge_msgs::RunPythonFile::Response&)>
+  typedef boost::function<bool(dynamic_graph_bridge_msgs::srv::RunPythonFile::Request&,
+                               dynamic_graph_bridge_msgs::srv::RunPythonFile::Response&)>
       runPythonFileCallback_t;
 
-  explicit Interpreter(ros::NodeHandle& nodeHandle);
+  explicit Interpreter(rclcpp::Node& nodeHandle);
 
   /// \brief Method to start python interpreter and deal with messages.
   /// \param Command string to execute, result, stdout, stderr strings.
@@ -41,18 +44,18 @@ class Interpreter {
 
  protected:
   /// \brief Run a Python command and return result, stderr and stdout.
-  bool runCommandCallback(dynamic_graph_bridge_msgs::RunCommand::Request& req,
-                          dynamic_graph_bridge_msgs::RunCommand::Response& res);
+  void runCommandCallback(dynamic_graph_bridge_msgs::srv::RunCommand::Request& req,
+                          dynamic_graph_bridge_msgs::srv::RunCommand::Response& res);
 
   /// \brief Run a Python file.
-  bool runPythonFileCallback(dynamic_graph_bridge_msgs::RunPythonFile::Request& req,
-                             dynamic_graph_bridge_msgs::RunPythonFile::Response& res);
+  void runPythonFileCallback(dynamic_graph_bridge_msgs::srv::RunPythonFile::Request& req,
+                             dynamic_graph_bridge_msgs::srv::RunPythonFile::Response& res);
 
  private:
   python::Interpreter interpreter_;
-  ros::NodeHandle& nodeHandle_;
-  ros::ServiceServer runCommandSrv_;
-  ros::ServiceServer runPythonFileSrv_;
+  rclcpp::Node& nodeHandle_;
+  rclcpp::Service<dynamic_graph_bridge_msgs::srv::RunCommand>::SharedPtr runCommandSrv_;
+  rclcpp::Service<dynamic_graph_bridge_msgs::srv::RunPythonFile>::SharedPtr runPythonFileSrv_;
 };
 }  // end of namespace dynamicgraph.
 
--- src/ros_interpreter.cpp
+++ src/ros_interpreter.cpp
@@ -3,38 +3,42 @@
 namespace dynamicgraph {
 static const int queueSize = 5;
 
-Interpreter::Interpreter(ros::NodeHandle& nodeHandle)
+  Interpreter::Interpreter(rclcpp::Node& nodeHandle)
     : interpreter_(), nodeHandle_(nodeHandle), runCommandSrv_(), runPythonFileSrv_() {}
-
-void Interpreter::startRosService() {
-  runCommandCallback_t runCommandCb = boost::bind(&Interpreter::runCommandCallback, this, _1, _2);
-  runCommandSrv_ = nodeHandle_.advertiseService("run_command", runCommandCb);
-
-  runPythonFileCallback_t runPythonFileCb = boost::bind(&Interpreter::runPythonFileCallback, this, _1, _2);
-  runPythonFileSrv_ = nodeHandle_.advertiseService("run_script", runPythonFileCb);
-}
-
-bool Interpreter::runCommandCallback(dynamic_graph_bridge_msgs::RunCommand::Request& req,
-                                     dynamic_graph_bridge_msgs::RunCommand::Response& res) {
-  interpreter_.python(req.input, res.result, res.standardoutput, res.standarderror);
-  return true;
-}
-
-bool Interpreter::runPythonFileCallback(dynamic_graph_bridge_msgs::RunPythonFile::Request& req,
-                                        dynamic_graph_bridge_msgs::RunPythonFile::Response& res) {
-  interpreter_.runPythonFile(req.input);
-  res.result = "File parsed";  // FIX: It is just an echo, is there a way to
-                               // have a feedback?
-  return true;
-}
-
-void Interpreter::runCommand(const std::string& command, std::string& result, std::string& out, std::string& err) {
-  interpreter_.python(command, result, out, err);
-  if (err.size() > 0) {
-    ROS_ERROR(err.c_str());
+  
+  void Interpreter::startRosService() {
+    runCommandCallback_t runCommandCb = std::bind(&Interpreter::runCommandCallback, this,
+                                                  std::placeholders::_1,
+                                                  std::placeholders::_2);
+    runCommandSrv_ = nodeHandle_.create_service<dynamic_graph_bridge_msgs::srv::RunCommand>
+      ("run_command", runCommandCb);
+    
+    runPythonFileCallback_t runPythonFileCb = std::bind(&Interpreter::runPythonFileCallback, this,
+                                                        std::placeholders::_1,
+                                                        std::placeholders::_2);
+    runPythonFileSrv_ = nodeHandle_.create_service<dynamic_graph_bridge_msgs::srv::RunPythonFile>
+      ("run_script", runPythonFileCb);
   }
-}
-
-void Interpreter::runPythonFile(std::string ifilename) { interpreter_.runPythonFile(ifilename); }
-
+  
+  void Interpreter::runCommandCallback(dynamic_graph_bridge_msgs::srv::RunCommand::Request& req,
+                                       dynamic_graph_bridge_msgs::srv::RunCommand::Response& res) {
+    interpreter_.python(req.input, res.result, res.standardoutput, res.standarderror);
+  }
+  
+  void Interpreter::runPythonFileCallback(dynamic_graph_bridge_msgs::srv::RunPythonFile::Request& req,
+                                          dynamic_graph_bridge_msgs::srv::RunPythonFile::Response& res) {
+    interpreter_.runPythonFile(req.input);
+    res.result = "File parsed";  // FIX: It is just an echo, is there a way to
+    // have a feedback?
+  }
+  
+  void Interpreter::runCommand(const std::string& command, std::string& result, std::string& out, std::string& err) {
+    interpreter_.python(command, result, out, err);
+    if (err.size() > 0) {
+      RCLCPP_ERROR(rclcpp::get_logger("dynamic_graph_bridge"),err.c_str());
+    }
+  }
+  
+  void Interpreter::runPythonFile(std::string ifilename) { interpreter_.runPythonFile(ifilename); }
+  
 }  // end of namespace dynamicgraph.
