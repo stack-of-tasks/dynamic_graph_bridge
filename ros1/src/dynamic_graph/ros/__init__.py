37a423ad209c48b9b646cc7519a911fc48cf39b1
MaximilienNaveau <maximilien.naveau@gmail.com>
2021-12-10 16:25:29 +0100

    remove ros1 and the ros2 subfolder. Set it to ros2

--- ros1/src/dynamic_graph/ros/__init__.py
+++
@@ -1,6 +0,0 @@
-# flake8: noqa
-from .ros import RosPublish as RosImport
-from .ros import RosSubscribe as RosExport
-from .ros_publish import RosPublish
-from .ros_subscribe import RosSubscribe
-from .ros_queued_subscribe import RosQueuedSubscribe
