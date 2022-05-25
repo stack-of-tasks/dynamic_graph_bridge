194d35dd3f91c37120734ed9e2ff02a2e6ace00d
Vladimir Petrik <vladimir.petrik@cvut.cz>
2022-05-25 15:28:52 +0200

    Switch to std pointer and fix wrong include path.

--- include/dynamic_graph_bridge/fwd.hh
+++ include/dynamic_graph_bridge/fwd.hh
@@ -9,12 +9,12 @@
 #ifndef DYNAMIC_GRAPH_BRIDGE_FWD_HH
 #define DYNAMIC_GRAPH_BRIDGE_FWD_HH
 
-#include <dynamic-graph/python/fwd.hh>
+#include <dynamic-graph/fwd.hh>
 
 namespace dynamicgraph {
   // classes defined in sot-core
   class Interpreter;
-  typedef shared_ptr<Interpreter> InterpreterPtr_t;
+  typedef std::shared_ptr<Interpreter> InterpreterPtr_t;
 }// namespace dynamicgraph
 
 #endif // DYNAMIC_GRAPH_PYTHON_FWD_HH
