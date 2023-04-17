9089b48b9ae6bb250405989268505357b11e3d2f
Olivier Stasse <ostasse@laas.fr>
2023-04-17 06:49:35 +0200

    Add python bindings for the test impl_test_sot_mock_device

---
+++ tests/impl_test_sot_mock_device-python-module-py.cc
@@ -0,0 +1,20 @@
+/*
+ * Copyright 2023, LAAS, CNRS
+ *
+ * Author:Olivier Stasse
+ *
+ * This file is part of dynamic_graph_bridge.
+ * This file is under the APACHE license 2.0
+ *
+ */
+#include <dynamic-graph/python/module.hh>
+
+#include "impl_test_sot_mock_device.hh"
+namespace dg = dynamicgraph;
+
+BOOST_PYTHON_MODULE(wrap) {
+  bp::import("dynmic_graph.sot.core.wrap");
+
+  dg::python::exposeEntity<ImplTestSotMockDevice,
+                           bp::bases<dg::sot::Device> >();
+}
