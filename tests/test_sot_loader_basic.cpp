9ad64efdb02c3724d79fb1fcc53b47d37abfa980
Olivier Stasse <ostasse@laas.fr>
2023-11-24 14:22:18 +0100

    Making pre-commit happy.

--- tests/test_sot_loader_basic.cpp
+++ tests/test_sot_loader_basic.cpp
@@ -6,23 +6,21 @@
 #include "dynamic_graph_bridge/ros.hpp"
 #include "dynamic_graph_bridge/sot_loader_basic.hh"
 
-
 namespace test_sot_loader_basic {
 int l_argc;
-char** l_argv;
-}
+char **l_argv;
+}  // namespace test_sot_loader_basic
 
 class MockSotLoaderBasicTest : public ::testing::Test {
  public:
   class MockSotLoaderBasic : public SotLoaderBasic {
    public:
-    MockSotLoaderBasic(const std::string &aNodeName=
-                       std::string("MockSotLoaderBasic")) :
-        SotLoaderBasic(aNodeName)
-    {}
+    MockSotLoaderBasic(
+        const std::string &aNodeName = std::string("MockSotLoaderBasic"))
+        : SotLoaderBasic(aNodeName) {}
     virtual ~MockSotLoaderBasic() {}
 
-    void checkStateVectorMap() {
+    void UniqueTest() {
       // This test makes sure that the class test_sot_loader_basic is able to
       // read parameters from the ROS-2 space.
       // It is suppose to read :
@@ -35,9 +33,7 @@ class MockSotLoaderBasicTest : public ::testing::Test {
       EXPECT_TRUE(aJoint1 == stateVectorMap_[0]);
       std::string aJoint2("joint2");
       EXPECT_TRUE(aJoint2 == stateVectorMap_[1]);
-    }
 
-    void testParseOptions() {
       // Void call
       int argc = 1;
       char argv1[30] = "mocktest";
@@ -61,50 +57,13 @@ class MockSotLoaderBasicTest : public ::testing::Test {
       // Check that the file is what we specified
       std::string finalname("libimpl_test_library.so");
       EXPECT_TRUE(finalname == dynamicLibraryName_);
-    }
 
-    void testInitializeRosNode() {
-      int argc = 1;
-      char *argv[1];
-      char argv1[30] = "mocktest";
-      argv[0] = argv1;
-      parseOptions(argc, argv);
       initPublication();
       EXPECT_TRUE(joint_pub_ != 0);
       initializeServices();
       EXPECT_TRUE(service_start_ != 0);
       EXPECT_TRUE(service_stop_ != 0);
-    }
 
-    void testJointStatesPublication() {
-      int argc = 1;
-      char *argv[1];
-      char argv1[30] = "mocktest";
-      argv[0] = argv1;
-      parseOptions(argc, argv);
-    }
-
-    void testInitialization() {
-      // Set input arguments
-      int argc = 2;
-      char *argv[2];
-
-      char argv1[30] = "mocktest";
-      argv[0] = argv1;
-      char argv2[60] = "--input-file=libimpl_test_library.so";
-      argv[1] = argv2;
-      parseOptions(argc, argv);
-
-      std::string finalname("libimpl_test_library.so");
-      EXPECT_TRUE(finalname == dynamicLibraryName_);
-
-      // Performs initializatio of libimpl_test_library.so
-      loadController();
-      EXPECT_TRUE(sotRobotControllerLibrary_ != 0);
-      EXPECT_TRUE(sotController_ != nullptr);
-    }
-
-    void test_start_stop_dg() {
       std::shared_ptr<std_srvs::srv::Empty::Request> empty_rqst;
       std::shared_ptr<std_srvs::srv::Empty::Response> empty_resp;
       start_dg(empty_rqst, empty_resp);
@@ -112,99 +71,48 @@ class MockSotLoaderBasicTest : public ::testing::Test {
 
       stop_dg(empty_rqst, empty_resp);
       EXPECT_TRUE(dynamic_graph_stopped_);
-    }
 
-    void test_cleanup() {
-      CleanUp();
-      EXPECT_TRUE(sotController_ == NULL);
-      std::cout << "sotController_: " << sotController_ << std::endl;
-
-      // Set input arguments
-      int argc = 2;
-      char *argv[2];
-
-      char argv1[30] = "mocktest";
-      argv[0] = argv1;
-      char argv2[60] = "--input-file=libimpl_test_library.so";
-      argv[1] = argv2;
-      parseOptions(argc, argv);
-
-      std::string finalname("libimpl_test_library.so");
-      EXPECT_TRUE(finalname == dynamicLibraryName_);
-
-      // Performs initializatio of libimpl_test_library.so
-      loadController();
       // Remove
       CleanUp();
     }
   };
 
  public:
-  MockSotLoaderBasic *mockSotLoaderBasic_ptr_;
+  std::shared_ptr<MockSotLoaderBasic> mockSotLoaderBasic_ptr_;
   unsigned int nb_tests_;
 
-  MockSotLoaderBasicTest() {
-    nb_tests_ = 0;
-  }
-      
+  MockSotLoaderBasicTest() { nb_tests_ = 0; }
+
   // For the set of tests coded in this file.
   static void SetUpTestCase() {
-    
-    rclcpp::init(test_sot_loader_basic::l_argc,
-                 test_sot_loader_basic::l_argv);
+    rclcpp::init(test_sot_loader_basic::l_argc, test_sot_loader_basic::l_argv);
   }
 
   // For each test specified in this file
-  static void TearDownTestCase() {
-    rclcpp::shutdown();
-  }
-  
+  static void TearDownTestCase() { rclcpp::shutdown(); }
+
   // For each test specified by TEST_F
   void SetUp() {
     std::string aSotLoaderBasicName("MockSotLoaderBasic" +
                                     std::to_string(nb_tests_));
-    mockSotLoaderBasic_ptr_ = new MockSotLoaderBasic(aSotLoaderBasicName);
+    mockSotLoaderBasic_ptr_ =
+        std::make_shared<MockSotLoaderBasic>(aSotLoaderBasicName);
     mockSotLoaderBasic_ptr_->initialize();
   }
-  
-  // For each test specified by TEST_F
-  void TearDown() {
-    delete mockSotLoaderBasic_ptr_;
-    mockSotLoaderBasic_ptr_ = nullptr;
-    nb_tests_++;
-  }
 
+  // For each test specified by TEST_F
+  void TearDown() { nb_tests_++; }
 };
 
-TEST_F(MockSotLoaderBasicTest, TestReadingParameters) {
-  mockSotLoaderBasic_ptr_->checkStateVectorMap();
-}
-
-TEST_F(MockSotLoaderBasicTest, ParseOptions) {
-  mockSotLoaderBasic_ptr_->testParseOptions();
-}
-
-TEST_F(MockSotLoaderBasicTest, TestInitialization) {
-  mockSotLoaderBasic_ptr_->testInitialization();
-}
-
-TEST_F(MockSotLoaderBasicTest, TestStartStopDG) {
-  mockSotLoaderBasic_ptr_->test_start_stop_dg();
-}
-
-TEST_F(MockSotLoaderBasicTest, TestInitializeRosNode) {
-  mockSotLoaderBasic_ptr_->testInitializeRosNode();
-}
-
-TEST_F(MockSotLoaderBasicTest, CleanUp) {
-  mockSotLoaderBasic_ptr_->test_cleanup();
+TEST_F(MockSotLoaderBasicTest, UniqueTest) {
+  mockSotLoaderBasic_ptr_->UniqueTest();
 }
 
 int main(int argc, char **argv) {
   ::testing::InitGoogleTest(&argc, argv);
   test_sot_loader_basic::l_argc = argc;
   test_sot_loader_basic::l_argv = argv;
-  
+
   int r = RUN_ALL_TESTS();
 
   return r;
